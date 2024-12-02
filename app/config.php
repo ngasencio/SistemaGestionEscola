bd23eecf0eb4b5bff4ff80831f346a8dbb6ba61f
ngasencio <81368379+ngasencio@users.noreply.github.com>
2024-12-01 22:48:01 -0300

    Initial commit

---
+++ app/config.php
@@ -0,0 +1,14 @@
+<?php 
+//Variables que se ocupan en todas las partes
+
+
+
+define('SERVIDOR', "localhost");
+define('USUARIO', "root");
+define('PASSWORD', "");
+define('BD', "sisgestionescolar");
+
+
+
+
+
