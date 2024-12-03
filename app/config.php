62b015aa3f9c393954d0e70f9553aa0349cd6e08
ngasencio <81368379+ngasencio@users.noreply.github.com>
2024-12-02 23:21:20 -0300

    Comint

--- app/config.php
+++ app/config.php
@@ -1,14 +1,35 @@
 <?php 
-//Variables que se ocupan en todas las partes
+
+define('SERVIDOR','localhost');
+define('USUARIO','root');
+define('PASSWORD','');
+define('BD','sisgestionescolar');
+
+define('APP_NAME','Sistema Gestión Escolar');
+define('APP_URL','http://localhost/SistemaGestionEscolar');
+define('KEY_API_MAPS','');
+
+
+$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
+
+/// Conexión
+
+try{
+    $pdo= new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    ///echo "conexión exitosa a la base de datos";
+} catch (PDOException $e) {
+    echo "error no se pudo conectar a la base de datos";
+}
 
 
 
-define('SERVIDOR', "localhost");
-define('USUARIO', "root");
-define('PASSWORD', "");
-define('BD', "sisgestionescolar");
+/// Hora y Fecha
+date_default_timezone_set('America/Santiago');
+$fechahora = date(format:'Y-m-d H:i:s');
+$fecha_actual = date(format:'Y-m-d');
+$fecha_dia = date(format:'d');
+$fecha_mes = date(format:'m');
+$fecha_ano = date(format:'Y');
 
 
-
-
-
+?>
--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php   echo "Hola Mundo" ?>
+
+
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <br>
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <h1>Vista Principal</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
